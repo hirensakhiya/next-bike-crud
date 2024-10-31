40ea609f8ec3b6f8534758c84e87da8a5b95140d
hirensakhiya <hirenh.sakhiya@gmail.com>
2024-10-31 17:15:55 +0530

    change email to username for login

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -17,8 +17,8 @@ const Login = () => {
             <div className="rounded overflow-hidden shadow-lg p-10 mt-5">
                 <h5 className="mb-5 text-2xl font-bold tracking-tight text-gray-900 ">Fictional Bike Store</h5>
                 <div className="mb-5">
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="name@flowbite.com" {...register("email", { required: "This field is required" })} />
+                    <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 ">Username</label>
+                    <input type="text" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="name@flowbite.com" {...register("email", { required: "This field is required" })} />
                     {errors.email && (
                         <span className="block text-red-400 mb-2">
                             {errors.email.message}
@@ -26,7 +26,7 @@ const Login = () => {
                     )}
                 </div>
                 <div className="mb-5">
-                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">Your password</label>
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
                     <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " {...register("password", { required: "This field is required" })} />
                     {errors.password && (
                         <span className="block text-red-400 mb-2">
